03a9ebad0f63594a53d5c32d0eeda4e6321d8c0e
kamalabot <kamaljp@gmail.com>
2023-07-31 11:58:18 +0530

    updated the styles of the app and project pages

--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
 
+# Historical Timelines in Django Land
+historical_timelines = [
+    "1840 - Founding",
+    "1895 - First Rails",
+    "1950 - Django Landmark",
+    "2005 - Django Framework",
+]
+
+# Geographical Anomalies in Django Land
+geographical_anomalies = [
+    "Rainbow Valley",
+    "Misty Peaks",
+    "Enchanted Caves",
+    "Whispering Woods",
+]
+
+# Condiments of Django Land
+condiments = [
+    "Whisker Sauce",
+    "Spicyberry Jam",
+    "Meadow Honey",
+    "Forest Truffle",
+]
+
+# Innovative Products by Django People
+innovative_products = [
+    "Skyglider Drone",
+    "EcoHarvest Machine",
+    "DigiGlobe",
+    "SwiftCycle",
+]
+
+lists = [
+    "historical_timelines",
+    "geographical_anomalies",
+    "condiments",
+    "innovative_products",
+]
 # Create your views here.
 
 def APP_INDEX(request):
-    return render(request, 'single_app.html')
+    context = {
+        "lists":lists,
+        "inno_pdts":innovative_products,
+        "condiments":condiments,
+        "anoms":geographical_anomalies,
+        "timelines":historical_timelines
+    }
+    return render(request, 'single_app.html',context)
--- project_1/project_1/views.py
+++ project_1/project_1/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render, redirect
 
+# Places to Visit in Django Land
+places_to_visit = [
+    "Django Park",
+    "View Point",
+    "ORM Lake",
+    "Admin Gardens",
+]
+
+# Things to Do in Django Land
+things_to_do = [
+    "Hiking Trails",
+    "Django Falls",
+    "Sunset Ridge",
+    "DjangoCon",
+]
+
+# Famous People of Django Land
+famous_people = [
+    "John Doe - Dev",
+    "Jane Smith - Adv",
+    "Michael Johnson",
+    "Sarah Williams",
+]
+
+# Tales of Django Land
+tales_of_django = [
+    "Django's Legacy",
+    "Django's Forest",
+    "Django's Journey",
+    "Django's River",
+]
+lists = [
+    "places_to_visit",
+    "things_to_do",
+    "famous_people",
+    "tales_of_django",
+]
+
 def INDEX(request):
-    return render(request, 'single_pjt.html')
+    context = {
+        "lists":lists,
+        "visit_places":places_to_visit,
+        "do_things":things_to_do,
+        "famous_people":famous_people,
+        "tales":tales_of_django
+    }
+    
+    return render(request, 'single_pjt.html',context)
 
