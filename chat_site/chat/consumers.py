e3283eed4904a0315acaba7b36b13a64b55422f6
kamalabot <kamaljp@gmail.com>
2023-09-17 19:37:23 +0530

    After completing the channels tutorials from the documentation

--- chat_site/chat/consumers.py
+++ chat_site/chat/consumers.py
@@ -1,4 +1,40 @@
 # chat/consumers.py
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
+
 """ import json
 
 from channels.generic.websocket import WebsocketConsumer
@@ -18,7 +54,7 @@ class ChatConsumer(WebsocketConsumer):
         #the data is echoed back...
         self.send(text_data=json.dumps({"message": message}))
 
- """
+### Here we are having synchronous websocketConsumer
 import json
 
 from asgiref.sync import async_to_sync
@@ -59,3 +95,4 @@ class ChatConsumer(WebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
+"""
--- chat_site/chat_site/settings.py
+++ chat_site/chat_site/settings.py
@@ -86,12 +86,14 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
