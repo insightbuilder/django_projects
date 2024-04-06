0bca227f4bd151ce374b379c02951030e0839594
kamalabot <kamaljp@gmail.com>
2024-04-06 22:03:50 +0530

    got the streaming working

--- warmup_fronted/prac/urls.py
+++ warmup_fronted/prac/urls.py
@@ -3,7 +3,9 @@ from .views import (
     welcome,
     get_name,
     get_req,
-    post_req
+    post_req,
+    streaming_view,
+    streamer_load,
 )
 
 urlpatterns = [
@@ -11,4 +13,6 @@ urlpatterns = [
     path("gn/<str:name>", get_name, name='gn'),
     path("get_req/", get_req, name='get_req'),
     path("post_req/", post_req, name='post_req'),
+    path("stream/", streaming_view, name='stream'),
+    path("load/", streamer_load, name='stload'),
 ]
--- warmup_fronted/prac/views.py
+++ warmup_fronted/prac/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import (
 )
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.http import StreamingHttpResponse
+import time
 
 def welcome(request):
     # return JsonResponse({'welcome': 'welcome'}, safe=False)
@@ -36,3 +37,24 @@ def post_req(request):
         return render(request, 'index.html', {"posted": name})
     else:
         return redirect('root')
+
+
+def streaming_view(request):
+    # Function to generate streaming content
+    def generate_content():
+        # Yield content in chunks
+        for i in range(10):
+            time.sleep(2)
+            yield f"Chunk {i}\n"
+    
+    # Create a streaming response with the generator function as content
+    response = StreamingHttpResponse(generate_content(), content_type="text/plain")
+    
+    # Optionally, you can set other response headers
+    response['Content-Disposition'] = 'attachment; filename="streaming_content.txt"'
+    
+    return response
+
+
+def streamer_load(request):
+    return render(request, 'streamer.html', {'test': "test"})
